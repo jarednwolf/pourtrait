85138118b8b27919c3f2294a25aff28b3d1c4808
Jared Wolf <jaredwolf@Jareds-MacBook-Pro.local>
2025-10-20 13:26:21 -0700

    build(ts): include src/types/**/*.d.ts in app tsconfig; add image module declarations; map public/* path; fix hero image import

---
+++ src/types/images.d.ts
@@ -0,0 +1,26 @@
+declare module '*.jpg' {
+  const src: import('next/dist/shared/lib/get-img-props').StaticImport
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: import('next/dist/shared/lib/get-img-props').StaticImport
+  export default src
+}
+
+declare module '*.png' {
+  const src: import('next/dist/shared/lib/get-img-props').StaticImport
+  export default src
+}
+
+declare module '*.webp' {
+  const src: import('next/dist/shared/lib/get-img-props').StaticImport
+  export default src
+}
+
+declare module '*.gif' {
+  const src: import('next/dist/shared/lib/get-img-props').StaticImport
+  export default src
+}
+
+
